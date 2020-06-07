bc8396bc291ba602a0fa4774bb96c402f0d27f53
janbruedigam <jan.bruedigam@t-online.de>
2020-06-07 14:59:10 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.2

---
+++ src/ConstrainedDynamicsVis.jl
@@ -0,0 +1,5 @@
+module ConstrainedDynamicsVis
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ConstrainedDynamicsVis
+using Test
+
+@testset "ConstrainedDynamicsVis.jl" begin
+    # Write your tests here.
+end
