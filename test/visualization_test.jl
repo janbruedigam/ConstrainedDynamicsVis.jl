f325afa4fa9c36666f45772622c4544ed1a71fcf
janbruedigam <jan.bruedigam@t-online.de>
2020-06-07 20:16:31 +0200

    Update tests.

--- test/visualization_test.jl
+++ test/visualization_test.jl
@@ -42,22 +42,23 @@ storagecylinder = simulate!(mechcylinder, 0.1, record = true)
 storagesphere = simulate!(mechsphere, 0.1, record = true)
 storagemesh = simulate!(mechmesh, 0.1, record = true)
 
-ConstrainedDynamicsVis.visualize(mechbox, storagebox, shapesbox; usebrowser = false)
-@test true
-ConstrainedDynamicsVis.visualize(mechbox, storagebox, shapesbox; usebrowser = true)
-@test true
 
-ConstrainedDynamicsVis.visualize(mechcylinder, storagecylinder, shapescylinder; usebrowser = false)
+if haskey(ENV,"RUNNER_OS") && !occursin("linux", lowercase(ENV["RUNNER_OS"]))
+    ConstrainedDynamicsVis.visualize(mechbox, storagebox, shapesbox; usebrowser = false)
+    @test true
+    ConstrainedDynamicsVis.visualize(mechcylinder, storagecylinder, shapescylinder; usebrowser = false)
+    @test true
+    ConstrainedDynamicsVis.visualize(mechsphere, storagesphere, shapessphere; usebrowser = false)
+    @test true
+    ConstrainedDynamicsVis.visualize(mechmesh, storagemesh, shapesmesh; usebrowser = false)
+    @test true
+end
+
+ConstrainedDynamicsVis.visualize(mechbox, storagebox, shapesbox; usebrowser = true)
 @test true
 ConstrainedDynamicsVis.visualize(mechcylinder, storagecylinder, shapescylinder; usebrowser = true)
 @test true
-
-ConstrainedDynamicsVis.visualize(mechsphere, storagesphere, shapessphere; usebrowser = false)
-@test true
 ConstrainedDynamicsVis.visualize(mechsphere, storagesphere, shapessphere; usebrowser = true)
-@test true
-
-ConstrainedDynamicsVis.visualize(mechmesh, storagemesh, shapesmesh; usebrowser = false)
 @test true
 ConstrainedDynamicsVis.visualize(mechmesh, storagemesh, shapesmesh; usebrowser = true)
 @test true
